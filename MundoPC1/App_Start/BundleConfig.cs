5b4a6131ebfe968e51bc697bb26f9bab9cae4d43
Jerry Anthony Gabriel Jacuinde <bandoler71@gmail.com>
2020-06-15 20:18:34 -0500

    Index,Delete,Create,Details and Edit for users

--- MundoPC1/App_Start/BundleConfig.cs
+++ MundoPC1/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace MundoPC1
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
                       "~/Content/styles.css"));
         }
     }
